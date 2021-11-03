6c173fa4438e6a77ffbabe63d0b1213c28f1e905
Quiet-Clicking-Sounds <55921097+Quiet-Clicking-Sounds@users.noreply.github.com>
2021-11-03 20:16:38 +0000

    Added Video import and export methods

--- IO.py
+++ IO.py
@@ -25,7 +25,6 @@ def load_image(file: pathlib.Path, RGB=True) -> numpy.ndarray:
         return cv2.imread(file.as_posix(), cv2.IMREAD_COLOR)
     return cv2.imread(file.as_posix(), cv2.IMREAD_GRAYSCALE)
 
-
 def export_to_image(url: pathlib.Path, data: numpy.ndarray):
     """
     Export image to disk using cv2 imwrite
@@ -34,3 +33,52 @@ def export_to_image(url: pathlib.Path, data: numpy.ndarray):
     """
     url.parent.mkdir(parents=True, exist_ok=True)  # make sure theres somewhere to save the image
     cv2.imwrite(url.as_posix(), data)
+
+
+def load_video(file:pathlib.Path) -> numpy.ndarray:
+    try:
+        capture = cv2.VideoCapture(file.__str__())
+        frame_count = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        buffer = numpy.empty((frame_count, frame_height, frame_width, 3))
+
+        frame = 0
+        return_val = True
+        while frame < frame_count and return_val:
+            return_val, buffer[frame] = capture.read()
+            frame += 1
+        capture.release()
+        print(f'Image: {buffer.shape}')
+        return buffer
+    except FileNotFoundError as fnf:
+        print(f'FileNotFoundError: \n\t\t{file}  \n{fnf}')
+        exit(1)
+
+def export_video(url: pathlib.Path, data: numpy.ndarray, fps=30, view=False):
+    url.parent.mkdir(parents=True, exist_ok=True)
+    if url.suffix != '.avi':
+        print(f"Fixing suffix without error: from {url.suffix}")
+        url = url.with_suffix('.avi')
+
+    try:
+        data = data.astype('uint8')
+        out = cv2.VideoWriter(filename=url.__str__(),
+                              fourcc=cv2.VideoWriter_fourcc(*'MJPG'),
+                              fps=fps,
+                              frameSize=(data.shape[2], data.shape[1]))
+        for frame in data:
+            out.write(frame)
+
+            if view:
+                cv2.imshow('frame', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+                time.sleep(1 / fps)
+
+        out.release()
+
+    except FileNotFoundError as fnf:
+        print(f'FileNotFoundError: \n\t\t{url}  \n{fnf}')
+        exit(1)
