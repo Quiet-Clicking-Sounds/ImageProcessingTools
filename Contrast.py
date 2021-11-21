8eb7ff0c9602cbdd464bd33316aa884f059e504a
Quiet-Clicking-Sounds <55921097+Quiet-Clicking-Sounds@users.noreply.github.com>
2021-11-21 08:24:18 +0000

    Removed redundant functions

--- Contrast.py
+++ Contrast.py
@@ -1,65 +1,67 @@
 import math
-from collections.abc import Callable
 
 import bottleneck
 import numpy
 from cv2 import cv2
 
+import IO
+
 
 class ImageCache:
-    def __init__(self, image: numpy.ndarray):
-        self.image: numpy.ndarray = image
-
+    def __init__(self, file_name: IO.Path):
+        # Load image on init
+        try:
+            self.image: numpy.ndarray = IO.load_image(file_name)
+        except AttributeError as ae:
+            if isinstance(file_name, numpy.ndarray):
+                self.image: numpy.ndarray = file_name
+            else:
+                raise ae
+        self.file_name = file_name
+        print(f'Input:  {hash(self)}    Base    {self.file_name}')
         self.moving_stdev = moving_stdev
         if len(self.image.shape) == 3:
             self.moving_stdev = rgb_moving_stdev
 
-        # dictionary used to cache image versions
-        self.dev_dict: dict[tuple[int, int, int], numpy.ndarray] = {}
+        # dictionary used to cache image versions '' Tuple[window, min_count]
+        self.dev_dict: dict[tuple[int, int], numpy.ndarray] = {}
 
-    def __call__(self, window: int, min_count: int = 1, axis: int = -1):
-        method_id = window, min_count, axis
+    def __call__(self, window: int, min_count: int = 1, double_std=False, cast_to=False, **kwargs):
+        method_id = window, min_count
         if method_id in self.dev_dict:
             return self.dev_dict[method_id]
-        self.dev_dict[method_id] = moving_stdev(self.image, window, min_count, axis)
+        stdev: numpy.ndarray = moving_stdev(self.image, window, min_count)
+        if double_std:
+            stdev = numpy.multiply(stdev, 2, )
+        if cast_to:
+            stdev = stdev.astype(numpy.uint8)
+        self.dev_dict[method_id] = stdev
         return self.dev_dict[method_id]
 
+    def __hash__(self):
+        return hash(str(self.image))
 
-def apply(function: Callable, array: numpy.ndarray, *args, **kwargs):
-    """
-    Apply a fuinction to an RGB image, uses cv2 split and merge, everything except function and array is
-    passed to the function as an argument
-    :param function: function to apply over each colour of the array
-    :param array: image array
-    :param kwargs: cl_args for the function
-    :return:
-    """
-    if len(array.shape) == 3:
-        return cv2.merge([function(a, *args, **kwargs) for a in cv2.split(array)])
-    return function(array, *args, **kwargs)
+    def output_file(self, sub_folder_name):
+        print(f'Output: {hash(self)}    {sub_folder_name}  {self.file_name}')
+        return self.file_name.parent / sub_folder_name / self.file_name.parts[-1]
 
 
-def moving_stdev(array: numpy.ndarray, window: int, min_count: int = 1, axis: int = -1) -> numpy.ndarray:
+def moving_stdev(array: numpy.ndarray, window: int, min_count: int = 1) -> numpy.ndarray:
     """
     Quickly apply a moving standard deviation calc over an array
     :param array: input array to apply over
     :param window: number of elements to apply the stdev method to must be > 1
     :param min_count:
-    :param axis:
     :return:
     """
-    return bottleneck.move_std(array,
-                               window=window,
-                               min_count=min_count,
-                               axis=-axis,
-                               )[:-window + 1, window - 1:]
+    return bottleneck.move_std(array, window=window, min_count=min_count, axis=1, )[:-window + 1, window - 1:]
 
 
-def rgb_moving_stdev(array: numpy.ndarray, window: int, min_count: int = 1, axis: int = -1) -> numpy.ndarray:
+def rgb_moving_stdev(array: numpy.ndarray, window: int, min_count: int = 1) -> numpy.ndarray:
     """
     apply moving stdev over an rgb array
     """
-    return cv2.merge([moving_stdev(a, window, min_count, axis) for a in cv2.split(array)])
+    return cv2.merge([moving_stdev(a, window, min_count) for a in cv2.split(array)])
 
 
 def resize_list_of_arrays(array_list: list[numpy.ndarray]) -> list[numpy.ndarray]:
@@ -68,6 +70,8 @@ def resize_list_of_arrays(array_list: list[numpy.ndarray]) -> list[numpy.ndarray
     :param array_list:
     :return:
     """
+    if len(set([i.shape for i in array_list])) <= 1:  # if arrays are already of equal size return
+        return array_list
     min_x = min([arr.shape[0] for arr in array_list])  # find minimum width
     min_y = min([arr.shape[1] for arr in array_list])  # find minimum height
     out_lst = []
@@ -80,45 +84,38 @@ def resize_list_of_arrays(array_list: list[numpy.ndarray]) -> list[numpy.ndarray
         dy1, dy2 = math.floor((a_y - min_y) / 2), math.ceil((a_y - min_y) / 2)
         out_lst.append(arr[dx1: -dx2, dy1: -dy2])
     first_item = out_lst[0]
-    comepleted_list = [arr for arr in out_lst if arr.shape == first_item.shape]
-    if len(array_list) != len(comepleted_list):
+    completed_array = [arr for arr in out_lst if arr.shape == first_item.shape]
+    if len(array_list) != len(completed_array):
         raise AssertionError("Input and output arrays are of different size")
-    return comepleted_list
+    return completed_array
 
 
-def combine_method_options(as_str=False) -> str or list[str]:
-    """ Function for use in print statements
-    :return:
-    """
-    part = ["sum", "avg"]
-    reversible_part = ["dist"]
-    part.extend(reversible_part)
-    part.extend([f'-{s}' for s in reversible_part])
-    if as_str:
-        return ', '.join(part)
-    return part
-
-
-def combine_array_list(array_list: list[numpy.ndarray], method: str = "sum") -> numpy.ndarray:
+def combine_array_list(array_list: list[numpy.ndarray], method: str = "avg") -> numpy.ndarray:
     """
     :param array_list:
     :param method: ['sum','avg','dist'] prepend '-' to reverse list before application
     :return:
     """
+    array_list = resize_list_of_arrays(array_list)
     if method[0] == '-':
         method = method[1:]
         array_list = array_list[::-1]
 
-    if method == 'sum':
-        return sum(array_list)
     if method == 'avg':
         return sum(array_list) / len(array_list)
     if method == 'dist':
-        """ dist outputs: array_list = [5,10,25] len(array_list = 3  
+        """ dist outputs: array_list = [5,10,25] len(array_list) = 3  
             ( 5*1/3*1 + 10*1/3*2 + 25*1/3*3 ) / 3 = 11.111 
             dist will output a combination closer to the final inputs given
             this can be called on a list of integers or floats for testing purposes
         """
-        return sum([arr * 1 / len(array_list) * i + 1 for i, arr in enumerate(array_list)])
+        dist_arr = sum([arr * 1 / len(array_list) * i + 1 for i, arr in enumerate(array_list)])
+        return numpy.multiply(dist_arr, 256 / numpy.max(dist_arr))
+    if method == 'pow':
+        """ pow outputs: array_list = [5,10,25] len(array_list = 3
+            
+        """
+        pow_arr = sum([numpy.power(arr, 1 + i / len(array_list)) for i, arr in enumerate(array_list)])
+        return numpy.multiply(pow_arr, 256 / numpy.max(pow_arr))
     else:
         raise ValueError(f"method argument invalid: {method}")
