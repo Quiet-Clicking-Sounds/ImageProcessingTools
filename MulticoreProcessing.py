606d743d64cff10b9bb14e143959f291d78e8056
Quiet-Clicking-Sounds <55921097+Quiet-Clicking-Sounds@users.noreply.github.com>
2022-02-13 12:32:56 +0000

    reformatted

--- MulticoreProcessing.py
+++ MulticoreProcessing.py
@@ -1,14 +1,13 @@
 # --------------------------------------------------------------------------------------------------------------
 # ---------------------------Multi Processing Function ---------------------------------------------------------
 # --------------------------------------------------------------------------------------------------------------
-from multiprocessing import Pool
-from os import cpu_count
+import multiprocessing as mp
 from collections.abc import Callable
+from os import cpu_count
 
 core_count = max(cpu_count() - 2, 1)
 
 
 def quick_pool(function: Callable, data_list: list) -> list:
-    with Pool(core_count) as p:
+    with mp.Pool(core_count) as p:
         return p.map(function, data_list)
-
