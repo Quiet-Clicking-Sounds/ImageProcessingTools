6803b3b61751de802e07dbeffac3d1554b0ac306
Quiet-Clicking-Sounds <55921097+Quiet-Clicking-Sounds@users.noreply.github.com>
2022-02-13 12:31:22 +0000

    reformatted with updated function names, removed leftover variables from testing

--- IO.py
+++ IO.py
@@ -1,38 +1,41 @@
-import pathlib
 from pathlib import Path
 
-from cv2 import cv2
 import numpy
+from cv2 import cv2
+
+file_extensions = ('.jpg', '.jpeg', '.gif')
+tk_file_extensions = (
+    ('Image Files', ' '.join([f'*{a}' for a in file_extensions]))
+)
 
 
-def get_list_of_files(directory: pathlib.Path) -> list[pathlib.Path]:
-    """ Return a list of files below the given directory """
+def list_files_in_directory_tree(directory: Path) -> list[Path]:
+    """ Return a list of files below the given directory, searches sub-folders """
     assert directory.is_dir()
     file_list = []
     for item in directory.iterdir():
         if item.is_file():
             file_list.append(item)
         elif item.is_dir():
-            file_list.extend(get_list_of_files(item))
+            file_list.extend(list_files_in_directory_tree(item))
     return file_list
 
 
-test_file_list: list[pathlib.Path] = get_list_of_files(pathlib.Path('TestFiles'))
-
-
-def load_image(file: pathlib.Path, rgb=True) -> numpy.ndarray:
+def load_image(file: Path, rgb=True) -> numpy.ndarray:
     """ get image from disk using cv2 """
+    if file.suffix not in ['.jpg', '.jpeg', '.gif']:
+        raise FileExistsError
     if rgb:
         return cv2.imread(file.as_posix(), cv2.IMREAD_COLOR)
     return cv2.imread(file.as_posix(), cv2.IMREAD_GRAYSCALE)
 
 
-def export_image(url: pathlib.Path, data: numpy.ndarray):
+def export_image(url: Path, data: numpy.ndarray):
     """
-    Export image to disk using cv2 imwrite
-    :param url: url to place the file, including extension "here/this.jpg"
-    :param data: numpy array containing image data
-    """
+        Export image to disk using cv2.imwrite
+        :param url: url to place the file, including extension "here/this.jpg"
+        :param data: numpy array containing image data
+        """
     url.parent.mkdir(parents=True, exist_ok=True)  # make sure theres somewhere to save the image
     cv2.imwrite(url.as_posix(), data)
 
